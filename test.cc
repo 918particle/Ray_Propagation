bffd1126d7dc09935e43bcb259f9164639516900
Jordan C Hanson <918particle@gmail.com>
2018-05-15 13:56:13 -0700

    Catching up after semester ending.

--- test.cc
+++ test.cc
@@ -9,13 +9,13 @@ int main(int argc, char **argv)
 	float n_ice = atof(argv[1]);
 	float z_0 = atof(argv[3]);
 	float C_0 = atof(argv[4]);
-	// float C_1 = atof(argv[5]);
-	float C_1 = sqrt((n_ice/C_0)*(n_ice/C_0)-1.0);
+	float C_1 = atof(argv[5]);
+	// float C_1 = sqrt((n_ice/C_0)*(n_ice/C_0)-1.0);
 	float b = 2.0 * n_ice;
 	float c = n_ice*n_ice - 1/C_0/C_0;
 	if(c>0)
 	{
-		for(float z=-500.0;z<=-100.0;z+=0.5)
+		for(float z=-500.0;z<=0.0;z+=0.5)
 		{
 			float gamma = delta_n * exp(z/z_0);
 			float y_0 = 1.0/C_0 * 1.0/sqrt(c) * z_0;
