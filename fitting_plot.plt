281abe61ecacccdd226262ff866d39f26c720824
Jordan C Hanson <918particle@gmail.com>
2018-05-18 15:18:38 -0700

    Almost have C_0 worked out.

--- fitting_plot.plt
+++ fitting_plot.plt
@@ -1,9 +1,11 @@
 set grid
 set datafile separator ","
 set pointsize .4
-f(x) = 1.0/a * tan(x) - (b*x)
-fit [-2:2] f(x) 'linear.dat' via a,b
+#f(x) = 1.0/n * tan(x) + b*x
+b = 1
+n = 1.5
+#fit [-2:2] f(x) 'linear.dat' via n,b
 set yrange [-5:5]
 set xrange [-3.14159/2.0:3.14159/2.0]
 
-plot 'linear.dat' w p pt 6, f(x)
+plot 'linear.dat' w p pt 6
--- test.cc
+++ test.cc
@@ -10,17 +10,16 @@ int main(int argc, char **argv)
 	float z_0 = 35.0;
 	float C_1 = 0.0;
 	float b = 2.0 * n_ice;
-	for(float C_0=-10;C_0<10;C_0+=0.01)
+	for(float C_0=0.0;C_0<10;C_0+=0.01)
 	{
 		float c = n_ice*n_ice - 1.0/C_0/C_0;
 		float y_0 = 1.0/C_0 * 1.0/sqrt(c) * z_0;
 		if(c>0)
 		{
-			float z_i = -20.0;
-			// for(float z_i=-75.0;z_i<-2;z_i+=1)
-			// {
+			for(float z_i=-100.0;z_i<-25;z_i+=2)
+			{
 				float n = n_ice - delta_n*exp(z_i/z_0);
-				float z_f = z_i+2.0;
+				float z_f = z_i+1.0;
 				float c = n_ice*n_ice - 1/C_0/C_0;
 				float gamma_i = delta_n * exp(z_i/z_0);
 				float k_i = log( (gamma_i) / (2.0*sqrt(c)*sqrt(gamma_i*gamma_i-b*gamma_i+c) - b*gamma_i + 2.0*c) );
@@ -31,9 +30,10 @@ int main(int argc, char **argv)
 				float delta_y = branch_1_f-branch_1_i;
 				float delta_z = z_f - z_i;
 				float angle = atan(delta_z/delta_y);
-				if(angle>0) cout<<angle<<","<<C_0-1.0/n_ice<<endl;
-				else cout<<angle<<","<<C_0+1.0/n_ice<<endl;
-			// }
+				cout<<angle<<","<<C_0<<endl;
+				// if(angle>0) cout<<angle<<","<<C_0-1.0/n<<endl;
+				// else cout<<angle<<","<<C_0+1.0/n<<endl;
+			}
 		}
 	}
 }
