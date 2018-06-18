14afb553f2978495353d534750235c6a7b37a5b9
Jordan C Hanson <918particle@gmail.com>
2018-06-18 10:55:38 -0700

    Discovered weird bug where signal turns around in air.

--- RunPropagator.cc
+++ RunPropagator.cc
@@ -7,14 +7,14 @@
 
 int main(int argc, char *argv[])
 {
-	std::pair<float,float> reflector_p(-35.0,0.05);
+	std::pair<float,float> reflector_p(-50.0,0.05);
 	std::pair<float,float> reflector_s(2,0.05);
 
 	//Seed rand()
 	srand(time(NULL));
 	int nrays = 100;
-	float angle_i = -10.0;
-	float angle_f = 10.0;
+	float angle_i = 70.0;
+	float angle_f = 80.0;
 	float delta_angle = 1.0;
 	Receiver *r = new Receiver();
 	r->InitializeReceiver(543.0,-2.0,0.0);
@@ -24,14 +24,14 @@ int main(int argc, char *argv[])
 		for(float angle = angle_i;angle<=angle_f;angle+=delta_angle)
 		{
 			Propagator *m = new Propagator();
-			m->InitializePropagator(0.0,-2.0,angle);
+			m->InitializePropagator(0.0,-300.0,angle);
 			m->AddReflector(reflector_p,reflector_s);
 			m->Propagate();
 			r->ReceiveRFRay(m);
-			// std::stringstream ss1,ss2;
-			// ss1<<i;
-			// ss2<<int(angle);
-			// m->ReadoutPath("data/sp_propagation_path_"+ss2.str()+"_"+ss1.str()+".dat");
+			std::stringstream ss1,ss2;
+			ss1<<i;
+			ss2<<int(angle);
+			m->ReadoutPath("data/sp_propagation_path_"+ss2.str()+"_"+ss1.str()+".dat");
 			delete m;
 		}
 	}
